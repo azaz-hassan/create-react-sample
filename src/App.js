a52edaf90692a8c5add437f4a0e508a51bb2763a
azaz-hassan-jafri <43964819+azaz-hassan-jafri@users.noreply.github.com>
2021-03-07 16:45:06 +0500

    Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      number: 4 + this.props.increment
+    }
+
+    this.props = props ;
+  }
+
+  handleChange = () => {
+    this.setState((prevState, prevProps) => {
+      return {number: prevState.number + prevProps.increment}
+    },
+      () => { console.log(this.state.number)}
+    )
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+
+          <p>
+            {this.state.number}
+          </p>
+          <button
+            onClick={this.handleChange}
+          >
+            Click Me
+          </button>
+        </header>
+      </div>
+    );
+  }
+}
 export default App;
